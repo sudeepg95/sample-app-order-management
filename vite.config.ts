88c9344fb8849aa3217960ce78ac6962b45f0e98
Sudeep G <sudeep.giri@equalexperts.com>
2026-04-30 14:33:50 +0530

    Update vite config with correct base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
+    base: "/sample-app-order-management/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
